85e587d8aa74304015971790541761704816370c
Лена Cтукало <lena.s26367@gmail.com>
2023-04-27 17:39:34 +0300

    hero start

---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,8 @@
+import css from './Hero.module.css'
+export function Hero(){
+    return <section className={css.container}>
+        <h1 className={css.title}>The Sky Is The Limit</h1>
+        <p  className={css.text}>We provide world class financial assistance</p>
+        <button  className={css.buttton}><p className={css.butttonText}>Read More</p></button>
+    </section>
+   }
--- src/components/layout/LayOut.jsx
+++ src/components/layout/LayOut.jsx
@@ -1,6 +1,8 @@
 import { Header } from "components/header/Header"
+import { Hero } from "components/hero/Hero"
 export function LayOut(){
     return <div className="">
      <Header/>
+     <Hero/>
    </div>
    }
