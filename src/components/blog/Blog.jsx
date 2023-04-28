665a064b6d7a8263d1768d68b6403b80b829e7c8
Лена Cтукало <lena.s26367@gmail.com>
2023-04-28 12:30:48 +0300

    blog tablet

--- src/components/blog/Blog.jsx
+++ src/components/blog/Blog.jsx
@@ -2,7 +2,7 @@ import { Image } from "./image/Image"
 import { Content } from "./content/Content"
 import css from'./Blog.module.css'
 export function Blog(){
-    return <div className={css.header} id="about">
+    return <div className={css.container} id="about">
      <Image/>
      <Content/>
    </div>
