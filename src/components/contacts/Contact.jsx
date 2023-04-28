5e5b97a7134b9f46cc1319aeb28592705924c5c4
Лена Cтукало <lena.s26367@gmail.com>
2023-04-29 02:28:02 +0300

    netlify form

--- src/components/contacts/Contact.jsx
+++ src/components/contacts/Contact.jsx
@@ -2,7 +2,7 @@ import { Image } from "./image/Image"
 import { Content } from "./content/Content"
 import css from "./Contact.module.css"
 export function Contact(){
-    return <section  id="about" className={css.container}>
+    return <section  id="contact" className={css.container}>
      <Image/>
      <Content/>
    </section>
