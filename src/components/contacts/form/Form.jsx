36d38cd3d13a26af1bc35708c36d5538d2d0b7a2
Лена Cтукало <lena.s26367@gmail.com>
2023-04-29 02:12:27 +0300

    netlify

--- src/components/contacts/form/Form.jsx
+++ src/components/contacts/form/Form.jsx
@@ -3,7 +3,7 @@ import css from './Form.module.css'
 import { TextInput } from './input/TextInput'
 export function Form(){
     
-    return <form className={css.form} name="contact v1" method='post' netlify onSubmit={()=>{}}>
+    return <form className={css.form} name="contact v1" method='post' data-netlify="true" onSubmit={()=>{}}>
         <input type='hidden' name='form-name' value='contact v1'/>
         <TextInput name='name' label='Enter your name' type='text'required={false}/>
         <TextInput name='email' label='Enter email*' type='email' required={true}/>
